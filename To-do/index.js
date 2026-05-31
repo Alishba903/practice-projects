a7f82f1529a95b4410589866572a14060f14755a
Alishba <a.alishba1002@gmail.com>
2026-05-31 21:19:33 +0500

    implement task deletion functionality and improve input validation in To-Do app

--- To-do/index.js
+++ To-do/index.js
@@ -2,17 +2,54 @@ const inputEl = document.getElementById("input-el");
 const addBtn = document.getElementById("add-btn");
 const taskList = document.getElementById("tasks-list");
 
+let tasks = [];
+
+window.onload = () =>{
+    tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    displayTasks();
+}
+
 addBtn.addEventListener("click", ()=>{
-    let inpVal = inputEl.value;
+    let inpVal = inputEl.value.trim();
     if(!inpVal){
         taskList.textContent = "The input field is empty!";
+        return;
     }
 
-    const li = document.createElement("li");
-    li.textContent = inpVal;
-    li.classList.add("task")
-    inputEl.value = "";
+    tasks.push(inpVal);
 
-    taskList.appendChild(li);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    displayTasks();
 
 })
+
+function displayTasks(){
+    taskList.innerHTML = "";
+    inputEl.value = "";
+
+    tasks.forEach((task, i)=>{
+
+    const li = document.createElement("li");
+    li.textContent = task;
+    li.classList.add("task");
+
+    const del = document.createElement("button");
+    del.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+    del.classList.add("del-btn");
+
+    del.addEventListener("click", ()=> deleteTodo(i))
+
+    li.appendChild(del)
+    taskList.appendChild(li);
+
+    })
+
+}
+
+function deleteTodo(index){
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    displayTasks();
+}
+
